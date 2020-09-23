d6e2e7e288a2484aaa324453cef23fbfdb75bc93
ArdeshirV <ArdeshirV@protonmail.com>
2020-09-23 12:39:08 +0330

    Rename source file

--- comma2nl.py
+++ comma2nl.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-# nl2comma - Convert Comma(s) in Text Files to New Lines, Version 1.0
+# comma2nl - Convert Comma(s) in Text Files to New Lines, Version 1.0
 # Copyright 2020 ArdeshirV@protonmail.com, Licensed under GPLv3+
 from sys import argv, exit
 
 
 def main(args):
+    if len(args) < 2:
+        print("\033[0;31mError: \033[1;31mYou sould"
+              " specify a file name to process!\033[0m")
+        return -1
     file_input = open(args[1], "r")
     contents = file_input.read()
     file_input.close()
@@ -30,4 +34,3 @@ def main(args):
 
 if __name__ == "__main__":
     exit(main(argv))
-
